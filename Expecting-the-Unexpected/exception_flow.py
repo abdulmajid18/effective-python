22dba01d5bb791f0b24e939ec89dab0b8e934015
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2023-09-14 08:40:51 +0000

    exception flow

---
+++ Expecting-the-Unexpected/exception_flow.py
@@ -0,0 +1,12 @@
+from typing import NoReturn
+
+
+def never_returns() -> NoReturn:
+    print("I am about to raise an exception")
+    raise Exception("This is always raised")
+    print("This line will never execute")
+    return "I won't be returned"
+
+
+if __name__ == '__main__':
+    never_returns()
