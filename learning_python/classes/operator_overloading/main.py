a2b5a8cbd7750c8d58f28b95889bd85317d85403
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2023-11-03 12:15:05 +0000

    operator overloading

---
+++ learning_python/classes/operator_overloading/main.py
@@ -0,0 +1,6 @@
+from number import  Number
+
+
+X = Number(10)
+Y  = X - 2
+print(Y)
---
+++ learning_python/classes/operator_overloading/number.py
@@ -0,0 +1,9 @@
+class Number:
+    def __init__(self, start):  # On Number(start)
+        self.data = start
+
+    def __sub__(self, other):  # On instance - other
+        return Number(self.data - other)  # Result is a new instance
+
+    def __str__(self):
+        return f"Number {self.data}"
