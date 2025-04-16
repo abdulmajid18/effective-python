bea4c9f7b821985bf46c5fe4a669751d78caed51
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-04-16 10:16:38 +0000

    basic decorator

---
+++ decorator/time_execution.py
@@ -0,0 +1,17 @@
+import time
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"{func.__name__} took {end - start:.4f} seconds")
+        return result
+    return wrapper
+
+@time_it
+def slow_function():
+    time.sleep(1)
+
+# Calling slow_function
+slow_function()
